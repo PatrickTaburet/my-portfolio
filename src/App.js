4c3601af3cdec31b653c3fc28d7cd256670f6a30
Patrick Taburet <taburet.patrick@gmail.com>
2024-11-22 19:28:59 +0100

    Global styles ok, first section in process.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import './App.css';
 import P5Sketch from './components/p5-sketch';
+import './global.css';
 
 function App() {
   return (
     <div className="App">
       <header className="header">
-        <h1>Taburet Patrick</h1>
-        <h2>Web developpement</h2>
+        <a href="">My Projects</a> / <a href="">About me</a> / <a href="">Contact</a>
       </header>
-      <div className='container'>
-        <P5Sketch />
-        <div className='sliderContainer'>
-          <p>lines</p>
-          <input type="range" min="1" max="60" defaultValue="5" step="1" id="lineSlider" />
-        </div>
-      </div>
-      <main>
+
+
+
+      <main className='container'>
+        <section>
+          <div className='section1Content'>
+            <h1>Taburet Patrick</h1>
+            <h2>Web developpement</h2>
+          </div>
+          <P5Sketch />
+          <div className='sliderContainer'>
+            <p>lines</p>
+            <input type="range" min="1" max="60" defaultValue="5" step="1" id="lineSlider" />
+          </div>
+        </section>
        
       </main>
 
