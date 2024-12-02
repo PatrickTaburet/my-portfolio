335d4f386e1c5e70480350bd62fb32954f595799
Patrick Taburet <taburet.patrick@gmail.com>
2024-12-03 00:35:20 +0100

    Refactoring and optimization.

--- src/components/sketches-p5js/Sketch3.jsx
+++ src/components/sketches-p5js/Sketch3.jsx
@@ -19,9 +19,7 @@ const Sketch3 = ({ onCircleClick, launchMode, closedCircle }) => {
 
     return () => {
       window.removeEventListener('resize', handleResize);
-      if (p5InstanceRef.current) {
-        p5InstanceRef.current.remove();
-      }
+      p5InstanceRef.current?.remove();
     };
   }, []);
 
@@ -29,7 +27,8 @@ const Sketch3 = ({ onCircleClick, launchMode, closedCircle }) => {
     let circles = [];
     let storedCircles = [];
     let mediaProject1, mediaProject2;
-    let fade;
+    let circleSize;
+    const LERP_SPEED = 0.1;
 
     p.preload = () => {
       mediaProject1 = p.loadImage(NexusLabMedia);
@@ -42,8 +41,8 @@ const Sketch3 = ({ onCircleClick, launchMode, closedCircle }) => {
       p.setAttributes({ alpha: true });
       p.colorMode(p.HSB, 360, 100, 100, 1);
       p.frameRate(50);
-      fade = 0
 
+      circleSize = Math.min(p.width, p.height) * 0.2;
       circles.push(new Circle(0, "NexusLab", mediaProject1));
       circles.push(new Circle(p.PI, "Creative\nCoding", mediaProject2)); // Start the second circle at the opposite
       // circles.push(new Circle(p.PI/2, "test"));
@@ -60,9 +59,8 @@ const Sketch3 = ({ onCircleClick, launchMode, closedCircle }) => {
     };
 
     p.draw = () => {
-      // p.background(196, 58, 5);
+      // p.background(196, 58, 5);      
       p.clear(); 
-      console.log(launchMode, closedCircle );
       const isClosingCircle = circles.some(circle => circle.isClosing);
       // Calculate the distance of the mouse from the center of the screen
       const centerX = p.width / 2;
@@ -75,21 +73,29 @@ const Sketch3 = ({ onCircleClick, launchMode, closedCircle }) => {
       });
     };
 
+    p.windowResized = () => {
+      // Dynamix update of circleSize
+      circleSize = Math.min(p.width, p.height) * 0.2;
+    };
+
     p.mouseMoved = () => {
-      if (!p._renderer || !p._renderer.elt) {
-        return; 
-      }
-      let isHovering = false;
+      if (!p._renderer || !p._renderer.elt) return;
     
-      circles.forEach(circle => {
+      let isHovering = false;
+
+      for (let i = 0; i < circles.length; i++) {
+        const circle = circles[i];
         const isHovered = circle.checkHover(p.mouseX, p.mouseY);
+    
         if (circle.hovered !== isHovered) {
-          circle.hovered = isHovered; 
+          circle.hovered = isHovered;
         }
+    
         if (isHovered) {
           isHovering = true;
+          break;
         }
-      });
+      }
     
       p.cursor(isHovering ? p.HAND : p.ARROW);
     };
@@ -109,7 +115,6 @@ const Sketch3 = ({ onCircleClick, launchMode, closedCircle }) => {
                 onCircleClick(circle.text);
               }, 800);
             }
-            
           } else {
             // If the circle is collapsed, re-add all the circles stored in storedCircles
             circles = [circle, ...storedCircles];
@@ -121,7 +126,7 @@ const Sketch3 = ({ onCircleClick, launchMode, closedCircle }) => {
     
     class Circle {
       constructor(initialAngle, text, media) {
-        this.size = Math.min(p.width, p.height) * 0.2; 
+        this.size = circleSize; 
         this.angle = initialAngle;
         this.speed = 0.01;
         this.media = media;
@@ -140,40 +145,38 @@ const Sketch3 = ({ onCircleClick, launchMode, closedCircle }) => {
           // If the circle is enlarged, set its position progressively to the center of the canvas
           this.opacity = p.lerp(this.opacity, -0.5, 0.05); 
 
-          this.x = p.lerp(this.x, p.width / 2, 0.1);
-          this.y = p.lerp(this.y, p.height / 2, 0.1);
+          this.x = p.lerp(this.x, p.width / 2, LERP_SPEED);
+          this.y = p.lerp(this.y, p.height / 2, LERP_SPEED);
           this.size = p.lerp(this.size, Math.max(p.width, p.height) * 2, 0.03); // Progressive enlargement to fullscreen
         } else if (this.isClosing ){
-          console.log("iciiii");
-           // Calculer la position cible en fonction de l'angle initial
+
+          // Calculate the target position based on the initial angle
           const maxDistanceX = p.map(p.width, 300, 1800, p.width * 0.3, p.width * 0.2, true);
           const maxDistanceY = p.map(p.width, 300, 1800, p.height * 0.1, p.height * 0.15, true);
           
-          // Position cible en utilisant l'angle initial
-          this.x = p.lerp(this.x, p.width / 2 + p.cos(this.initialAngle) * maxDistanceX, 0.1);
-          this.y = p.lerp(this.y, p.height / 2 + p.sin(this.initialAngle) * maxDistanceY, 0.1);
+          // Target position using the initial angle
+          this.x = p.lerp(this.x, p.width / 2 + p.cos(this.initialAngle) * maxDistanceX, LERP_SPEED);
+          this.y = p.lerp(this.y, p.height / 2 + p.sin(this.initialAngle) * maxDistanceY, LERP_SPEED);
 
-          this.size = p.lerp(this.size, Math.min(p.width, p.height) * 0.2, 0.05);
+          this.size = p.lerp(this.size, circleSize, LERP_SPEED);
 
-          if (Math.abs(this.size - Math.min(p.width, p.height) * 0.2) < 1) {
-            this.isClosing = false; // Terminé
-            console.log("OVERRRR");
+          if (Math.abs(this.size - circleSize) < 1) {
+            this.isClosing = false;
           }
           
           
         } else if (isClosingCircle) {
-          // Immobilisation des cercles non sélectionnés pendant la fermeture d'un autre cercle
+          // Locking the unselected circles while closing another circle          
           const maxDistanceX = p.map(p.width, 300, 1800, p.width * 0.3, p.width * 0.2, true);
           const maxDistanceY = p.map(p.width, 300, 1800, p.height * 0.1, p.height * 0.15, true);
       
           this.x = p.width / 2 + p.cos(this.initialAngle) * maxDistanceX;
           this.y = p.height / 2 + p.sin(this.initialAngle) * maxDistanceY;
-          
         } else {
 
-           // Proportional orbital distance
-           let maxDistanceX = p.map(p.width, 300, 1800, p.width * 0.3, p.width * 0.2, true);
-           let maxDistanceY = p.map(p.width, 300, 1800, p.height * 0.1, p.height * 0.15, true);
+          // Proportional orbital distance
+          let maxDistanceX = p.map(p.width, 300, 1800, p.width * 0.3, p.width * 0.2, true);
+          let maxDistanceY = p.map(p.width, 300, 1800, p.height * 0.1, p.height * 0.15, true);
 
           // Orbital movment
           this.angle += this.speed;
@@ -184,45 +187,44 @@ const Sketch3 = ({ onCircleClick, launchMode, closedCircle }) => {
           this.y = p.height / 2 + p.sin(this.angle) * maxDistanceY;
 
           // Dynamic cicles size
-          let maxCircleSize = Math.min(p.width, p.height) * 0.2; 
-          let targetSize = this.hovered ? maxCircleSize * 2 : maxCircleSize;
+          let targetSize = this.hovered ? circleSize * 2 : circleSize;
           this.size = p.lerp(this.size, targetSize * perspective, 0.3);
           
           if (this.hovered) {
-            this.opacity = p.lerp(this.opacity, 1, 0.1); // Opacity increases smoothly on hover
+            this.opacity = p.lerp(this.opacity, 1, LERP_SPEED); // Opacity increases smoothly on hover
           } else {
-            this.opacity = p.lerp(this.opacity, 0, 0.1); // Opacity decreases smoothly when not hovered
+            this.opacity = p.lerp(this.opacity, 0, LERP_SPEED); // Opacity decreases smoothly when not hovered
           }
-          
-          // Constrain to canvas bounds
-          // this.x = p.constrain(this.x, this.size/2, p.width - this.size/2);
-          // this.y = p.constrain(this.y, this.size/2, p.height - this.size/2);
         }   
       }
-
       display() {
-        // Draw circles
+        this.drawCircle();
+        this.drawMedia();
+        this.drawText();
+      }
+
+      drawCircle() {
         p.fill(163, 93, 100);
         p.ellipse(this.x, this.y, this.size);
-        
+      }
+      drawMedia() {
         // Display project preview with fade effect
         if (this.media && this.opacity > 0) {
-          let maskedImage = this.createMaskedImage(this.media, this.size);
+          const maskedImage = this.createMaskedImage(this.media, this.size);
           p.push();
           p.tint(255, this.opacity); // Apply fade transition to image opacity
           p.image(maskedImage, this.x - this.size / 2, this.y - this.size / 2);
           p.pop();
-  
         }
-        
-        if(!this.isExpanded){
+      }
+      drawText() {
+        if (!this.isExpanded) {
           // Display project title
           p.textAlign(p.CENTER, p.CENTER);
           p.fill(0);
           p.textSize(this.size / 7);
           p.text(this.text, this.x, this.y);
-        }       
-      
+        }
       }
 
       createMaskedImage(img, size) {
