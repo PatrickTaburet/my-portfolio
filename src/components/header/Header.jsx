480d650f1b6712ac761cc4e5bf9c9e3a876fed62
Patrick Taburet <taburet.patrick@gmail.com>
2025-01-21 10:47:27 +0100

    SEO optimisations.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,50 +5,53 @@ import { useMobile } from '../../context/MobileContext';
 
 function Header({ scrollY, windowHeight, isProjectInfoVisible, sectionOffsets, scrollToSection, handleCloseFromHeader }) {
   const isMobile = useMobile(); 
-  
+
+  const headerColor = isMobile
+    ? ''
+    : (scrollY > windowHeight && scrollY <= window.innerHeight * 1.6 ? "#2BF7BC" : '');
+
   return (
     <header 
       className="header"
       style={{
-        color: isMobile
-          ? '' 
-          : (scrollY > windowHeight && scrollY <= window.innerHeight * 1.6 ? "#2BF7BC" : '')
+        color: headerColor
       }}
-      // style={{color:`${scrollY > windowHeight && scrollY <=  window.innerHeight * 1.6 ? "#2BF7BC" : ''}`}}
     >
       <AnimatedTitle timeout={"900"} direction="down" initiallyVisible={true}>
-        <button 
-           className={`link ${
-            isMobile
-              ? 'modeA' // Forcer modeA en mode mobile
+        <nav>
+          <button 
+            className={`link ${
+              isMobile
+                ? 'modeA' // Forcer modeA en mode mobile
+                : (isProjectInfoVisible && scrollY >= window.innerHeight * 1.6
+                  ? 'modeC'
+                  : ((scrollY > windowHeight && scrollY <= window.innerHeight * 1.6)
+                    || (isProjectInfoVisible && scrollY >= window.innerHeight * 1.6)
+                    ? 'modeB' : 'modeA'))
+            }`}
+            onClick={() => {
+              scrollToSection(sectionOffsets.section3Offset);
+              handleCloseFromHeader();              
+            }}
+          >
+            Projects 
+          </button> 
+            / 
+          <button 
+            className={`link ${
+              isMobile
+              ? 'modeA'
               : (isProjectInfoVisible && scrollY >= window.innerHeight * 1.6
                 ? 'modeC'
                 : ((scrollY > windowHeight && scrollY <= window.innerHeight * 1.6)
                   || (isProjectInfoVisible && scrollY >= window.innerHeight * 1.6)
                   ? 'modeB' : 'modeA'))
           }`}
-          onClick={() => {
-            scrollToSection(sectionOffsets.section3Offset);
-            handleCloseFromHeader();              
-          }}
-        >
-          Projects 
-        </button> 
-          / 
-        <button 
-          className={`link ${
-            isMobile
-            ? 'modeA'
-            : (isProjectInfoVisible && scrollY >= window.innerHeight * 1.6
-              ? 'modeC'
-              : ((scrollY > windowHeight && scrollY <= window.innerHeight * 1.6)
-                || (isProjectInfoVisible && scrollY >= window.innerHeight * 1.6)
-                ? 'modeB' : 'modeA'))
-        }`}
-          onClick={() => scrollToSection(sectionOffsets.section2Offset)}
-        >
-          Contact
-        </button>
+            onClick={() => scrollToSection(sectionOffsets.section2Offset)}
+          >
+            Contact
+          </button>
+        </nav>
       </AnimatedTitle>
     </header>
   );
